60a9c41b9bc2d0e085fa9c43cac9c5a55f9c31ee
Mitch <munch05b@gmail.com>
2025-04-29 15:44:48 -0600

    Update the bloom effect

--- shaders/config.glsl
+++ shaders/config.glsl
@@ -5,3 +5,4 @@
 #define WAVING_PLANTS // Determines if grass, crops, and leaves move in the wind
 #define TEXTURE_RESOLUTION 16 // Resolution of your texture pack. Determines resolution to use for shadows [1 2 4 8 16 32 64 128]
 #define FOG_DENSITY 5.0 // Controls how much fog there is [1.0 2.0 3.0 4.0 5.0 6.0 7.0 8.0 9.0 10.0]
+#define BLOOM_INTENSITY 1.0 // How bloom is the bloom? [0.0 0.2 0.4 0.6 0.8 1.0 1.2 1.4 1.6 1.8 2.0 2.2 2.4 2.6 2.8 3.0 3.2 3.4 3.6 3.8 4.0 4.2 4.4 4.6 4.8 5.0 5.2 5.4 5.6 5.8 6.0 6.2 6.4 6.6 6.8 7.0 7.2 7.4 7.6 7.8 8.0 8.2 8.4 8.6 8.8 9.0 9.2 9.4 9.6 9.8 10.0]
